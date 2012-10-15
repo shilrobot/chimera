2dfc146ed7d4323740bf3058760d0b19bb52443a
Scott Hilbert <github@shilbert.com>
2012-10-15 12:32:52 -0400

    Fix lint errors

--- chimera.py
+++ chimera.py
@@ -1342,10 +1342,10 @@ if __name__ == '__main__':
 
     # See http://www.py2exe.org/index.cgi/HowToDetermineIfRunningFromExe
     def main_is_frozen():
-        return (hasattr(sys, "frozen") or # new py2exe
-                hasattr(sys, "importers") # old py2exe
-                or imp.is_frozen("__main__")) # tools/freeze
-     
+        return (hasattr(sys, "frozen") or  # new py2exe
+                hasattr(sys, "importers")  # old py2exe
+                or imp.is_frozen("__main__"))  # tools/freeze
+
     def get_main_dir():
         if main_is_frozen():
             return os.path.dirname(sys.executable)
